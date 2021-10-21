35a54a18728439725ac0bd6b994c99944e878489
hwpoison <guichezero@gmail.com>
2021-10-21 04:22:28 -0300

    add col name config

--- gui.py
+++ gui.py
@@ -14,6 +14,7 @@ class Window:
 	def __init__(self, master):
 		self.main = master
 		self.actual_selection = None
+		self.actual_selection_idx = None
 		self.contacts_filename = "contacts.csv"
 		# vars
 		self.amount_files = StringVar()
@@ -29,9 +30,7 @@ class Window:
 		self.main.title("Whattasap! (by:hwpoison)")
 		self.main.geometry("300x550+600+100")
 		self.main.resizable(False, False)
-		self.load_contacts_from()
-		self.populate_contact_list()
-		
+		self.load_contact_box()
 		self.main.mainloop()
 	
 	def init_driver(self):
@@ -60,10 +59,10 @@ class Window:
 	def populate_contact_list(self):
 		self.contacts_list_box.delete(0, END)
 		for contact in pywsp.contacts.values():
-			nombre = f"{contact['nombre']} {contact['apellido']}"
-			if len(nombre) < 20:
-				nombre = nombre + ' '*(30-len(nombre))
-			self.contacts_list_box.insert(END, f"{nombre} {contact['telefono']}")
+			name = f"{contact[pywsp.config['NAME_COL_NAME']]} {contact[pywsp.config['LASTNAME_COL_NAME']]}"
+			if len(name) < 20:
+				name = name + ' '*(30-len(name))
+			self.contacts_list_box.insert(END, f"{name} {contact[pywsp.config['PHONE_COL_NAME']]}")
 
 	def check_driver_thread(self):
 		Thread(target=self.check_driver_loop).start()
@@ -78,7 +77,8 @@ class Window:
 				self.init_driver_btn.config(state=ACTIVE)
 
 	def load_contacts_from(self):
-			pywsp.load_contacts(self.contacts_filename)	
+			pywsp.contacts = {}
+			return pywsp.load_contacts(self.contacts_filename)
 
 	def driver_is_opened(self):
 		if not pywsp.browser:
@@ -88,39 +88,76 @@ class Window:
 			return False
 		return True
 
-	def reload_contacts_box(self):
-		print("[UI] Reloading contacts list.")
-		self.load_contacts_from()
-		self.populate_contact_list()
-	
+	def load_contact_box(self):
+		print("[UI] Loading contacts list.")
+		try:
+			self.load_contacts_from()
+			self.populate_contact_list()
+		except:
+			messagebox.showerror('Error', 'Problema al cargar los contactos del .csv, por favor revise la configuración.')
+
 	def on_select_contact(self, event):
 	  selection = event.widget.curselection()
 	  index = selection[0]
 	  value = event.widget.get(index)
 	  self.actual_selection = pywsp.contacts[index]
+	  self.actual_selection_idx = index
 	  print("[UI]Selected:", index,' -> ',value, self.actual_selection)
 
 	def get_msg_box_content(self):
 		return self.message_box.get("1.0","end-1c")
 
+	def format_message(self, contact):
+		try:
+			message = pywsp.format_message(self.get_msg_box_content(), contact)
+			return message
+		except:
+			messagebox.showerror('Oops!', 
+				'Problema al formatear el mensaje, revisa que las palabras claves coincidan con el nombre de alguna columna.')
+			return False		
+
+	def send_to(self, contact : dict, attachment : list):
+		print("[+]Sending message to", contact)
+		message = self.format_message(contact)
+		if not message:
+			return False
+
+		return pywsp.send_to(contact, message, attachment)
+
+
 	def send_to_selected(self):
 		if not self.driver_is_opened():
 			return False
-		print("[+]Sending message to", self.actual_selection)
-		pywsp.send_to(self.actual_selection, self.get_msg_box_content(), self.actual_attachment)
+		sent = self.send_to(self.actual_selection, self.actual_attachment)
+		if sent:
+			self.set_contact_color(self.actual_selection_idx)		
+		else:
+			self.set_contact_color(self.actual_selection_idx, color='red')
+
+		return sent
+
+	def set_contact_color(self, index, color='green'):
+		if color == 'green':
+			self.contacts_list_box.itemconfigure(index, bg="#00aa00", fg="#fff")
+		else:
+			self.contacts_list_box.itemconfigure(index, bg="#ff0000", fg="#fff")
 
 	def send_to_all(self):
 		if not self.driver_is_opened():
 			return False
 		print("[UI]Starting send to all.")
 		for idx, contact in pywsp.contacts.items():
-			sent = pywsp.send_to(contact, self.get_msg_box_content(), self.actual_attachment)
+			message = self.format_message(contact)
+			if not message:
+				break
+			sent = pywsp.send_to(contact, message, self.actual_attachment)
 			if sent:
-				self.contacts_list_box.itemconfigure(idx, bg="#00aa00", fg="#fff")
+				self.set_contact_color(idx)
 			else:
-				self.contacts_list_box.itemconfigure(idx, bg="#ff0000", fg="#fff")
+				self.set_contact_color(idx, color='red')
 		messagebox.showinfo("Listo!", "Finalizó el envío masivo.")
 		print("[UI]Massive sent Finished.")
+		return True
 
 	def load_gui(self):
 		# Buttons
@@ -137,7 +174,7 @@ class Window:
 		self.reload_contacts_btn = Button(
 			text="📓 Recargar lista de Contactos", 
 			width=50, 
-			command=self.reload_contacts_box)
+			command=self.load_contact_box)
 
 		self.files_attach = Button(
 			text="📎 Seleccionar adjuntos", 
@@ -196,6 +233,7 @@ class Window:
 		self.send_to_all.pack()
 		self.send_to_selected_btn.pack()
 
-print("Welcome :)")
+print("Holis! :)")
 app = Window(main)
+
 app.init_window()
--- pywsp.py
+++ pywsp.py
@@ -1,4 +1,3 @@
-# selenium dependencies
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
@@ -9,86 +8,61 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 
+import autoit
+
 import configparser
-
-# load autoit
-try:
-    import autoit
-except ModuleNotFoundError:
-    pass
-
 import time
 import os
 import re
 
+browser = None
+contacts = {}
+
+# common names for automatic detection :TODO
+commons_col_names = {
+    'name':['nombre','name', 'firstname'],
+    'lastname':['lastname', 'apellido'],
+    'phone':['phone', 'cellphone', 'teléfono', 'tel', 'whatsapp']
+}
 
 
-filedialog_title = 'Open' # defaut
+HOME_URL = "https://web.whatsapp.com/"
+
+config = {
+    # default values
+   'FILEDIALOG_TITLE': 'Open',
+   'NAME_COL_NAME': 'name',
+   'LASTNAME_COL_NAME': 'lastname',
+   'PHONE_COL_NAME': 'phone' 
+}
 
 # read config.ini
 config_load = configparser.ConfigParser()
 config_load.read('config.ini')
 if 'MISC' in config_load.sections():
     print("[+]reading config.ini")
-    if dialog_title:= config_load['MISC'].get('DIALOGFILE_TITLE'):
-        filedialog_title = dialog_title
+    misc = config_load['MISC']
+    if dialog_title:= misc.get('DIALOGFILE_TITLE'):
+        config['FILEDIALOG_TITLE'] = dialog_title
+    # default col names
+    if phone:= misc.get('PHONE_COL'):
+        config['PHONE_COL_NAME'] = phone
+    if lastname:= misc.get('LASTNAME_COL'):
+        config['LASTNAME_COL_NAME'] = lastname
+    if name:= misc.get('NAME_COL'):
+        config['NAME_COL_NAME'] = name
 
+# load driver path 
 chrome_default_path = os.getcwd() + '/driver/chromedriver' + \
     ('.exe' if os.sys.platform == 'win32' else '')  # xD
-
-HOME_URL = "https://web.whatsapp.com/"
-
-browser = None
-contacts = {}
 
 
 def sanitize_phone(phone):
     return phone.translate(str.maketrans({' ': '', '+': ''}))
 
-def write_message(number, message_text):
-    print(f"[+]Sending message to {number} with the text:{message_text}!")
-    js = """
-        // Open chat and write something to a phone number
-        const openChat = (phone_number, message='') => {
-            var wsp_msg_url = `https://web.whatsapp.com/send?phone=${ phone_number }&text=${ message }`
-            const open_chat = document.createElement('a')
-            open_chat.id = 'wsp_chat_'
-            open_chat.href = wsp_msg_url
-            document.body.appendChild(open_chat)
-            open_chat.click()
-        };""" f"openChat('{sanitize_phone(number)}', '{message_text}')"
-    browser.execute_script(js)
-
-
-def load_file(attachment=[]):
-    files = list(map(lambda f: f"\"{f}\"", attachment))
-    print("[+]Prepared for send ", files)
-
-    attach_btn_click = """
-        boton_adjuntar_imagen = document.querySelector('[data-testid="attach-image"]')
-        boton_adjuntar = document.querySelector('[aria-label="Adjuntar"]')
-        boton_adjuntar.click()
-    """
-    browser.execute_script(attach_btn_click)
-    upping = ActionChains(browser)\
-        .send_keys(Keys.ARROW_DOWN)\
-        .send_keys(Keys.ENTER)\
-        .perform()
-    time.sleep(1.5)
-    try:
-        # focus dialog file to write dirs.
-        autoit.control_focus(filedialog_title, "Edit1")
-        autoit.control_set_text(filedialog_title, "Edit1", " ".join(files))
-        autoit.control_click(filedialog_title, "Button1")
-        return True
-    except:
-        # if fails, close filedialog and attach dropdown pressing esc * 2
-        autoit.send("{ESC}")
-        autoit.send("{ESC}")
-        return False
-
 class ChatBoxHandle:
-
+    """ Functions to handle whatsapp web chat box 
+    """
     def confirm_send():
         js = """
             var btn_send = document.querySelector('[data-testid="send"]')
@@ -113,7 +87,50 @@ class ChatBoxHandle:
         """
         return browser.execute_script(js)
 
+    def attach_file(attachment=[]):
+        files = list(map(lambda f: f"\"{f}\"", attachment))
+        print("[+]Prepared for send ", files)
+
+        attach_btn_click = """
+            boton_adjuntar_imagen = document.querySelector('[data-testid="attach-image"]')
+            boton_adjuntar = document.querySelector('[aria-label="Adjuntar"]')
+            boton_adjuntar.click()
+        """
+        browser.execute_script(attach_btn_click)
+        upping = ActionChains(browser)\
+            .send_keys(Keys.ARROW_DOWN)\
+            .send_keys(Keys.ENTER)\
+            .perform()
+        time.sleep(1.5)
+        try:
+            # focus dialog file to write dirs.
+            autoit.control_focus(config['FILEDIALOG_TITLE'], "Edit1")
+            autoit.control_set_text(config['FILEDIALOG_TITLE'], "Edit1", " ".join(files))
+            autoit.control_click(config['FILEDIALOG_TITLE'], "Button1")
+            return True
+        except:
+            # if fails, close filedialog and attach dropdown pressing esc * 2
+            autoit.send("{ESC}")
+            autoit.send("{ESC}")
+            return False
+
+    def write_message(number, message_text):
+        print(f"[+]Sending message to {number} with the text:{message_text}!")
+        js = """
+            // Open chat and write something to a phone number
+            const openChat = (phone_number, message='') => {
+                var wsp_msg_url = `https://web.whatsapp.com/send?phone=${ phone_number }&text=${ message }`
+                const open_chat = document.createElement('a')
+                open_chat.id = 'wsp_chat_'
+                open_chat.href = wsp_msg_url
+                document.body.appendChild(open_chat)
+                open_chat.click()
+            };""" f"openChat('{sanitize_phone(number)}', '{message_text}')"
+        browser.execute_script(js)
+
 class NotificationTasteHandle:
+    """ Functions to handle taste notifications 
+    """
 
     def is_showing():
         js = """
@@ -128,6 +145,8 @@ class NotificationTasteHandle:
         return browser.execute_script(js)
 
 class ModalHandle:
+    """ Functions to handle modal 
+    """
 
     def getElement():
         # return modal (confirmation, error, etc) content like str
@@ -188,52 +207,58 @@ def whatsapp_login(headless=False):
     browser.maximize_window()
     print('[+]Driver initialized...')
 
-
-def filter_keywords(message):
-    keywords = re.findall('.(\$\w+)(?:.|$)', message)  # all $\w+ ocurrences
-    return keywords
+def format_message(message : str , keywords : dict):
+    # Substitute special keys ocurrences
+    new_message = message
+    keys = re.findall('.(\$\w+)(?:.|$)', message)  # all $\w+ ocurrences
+    for key in keys:
+        key = key.strip() # to replace reference: $(value)
+        new = keywords.get(key[1:]) # new value
+        # replace key $... for contact[key without $]
+        if not new:
+            raise Exception('[+]Problem to get column value.')
+        else:
+            new_message = new_message.replace(
+                key, new)
+    return new_message
 
 def send_to(contact, message, attachment):
     if not contact or not browser:
         return False
     try:
-        phone = contact['telefono']
+        phone = contact[config['PHONE_COL_NAME']]
         new_message = message
         print("="*30)
         print("\n[+] Sending message to ", phone)
-        # Substitute special keys ocurrences
-        for key in filter_keywords(message):
-            key = key.strip()
-            new = contact.get(key[1:])
-            # replace key $... for contact[key without $]
-            new_message = new_message.replace(
-                key, new if new else '<error>')
+
+        #new_message = format_message(new_message)
+
         # write the message
-        write_message(phone, new_message)
+        ChatBoxHandle.write_message(phone, new_message)
         # wait 1 sec 
         time.sleep(1)
         # verify if is a invalidPhone
         if ModalHandle.invalidPhone():
             # confirm modal and cancel send
             ModalHandle.confirm()
-            raise Exception("[x] Teléfono invalido! :(")
+            raise Exception("[x] Invalid Phone!")
 
         # if modal is not open
         if not ModalHandle.isOpened():
             # procced to attach file
             if attachment:
                     time.sleep(1.5)
-                    load_files = load_file(attachment)
+                    load_files = ChatBoxHandle.attach_file(attachment)
                     # wait to finish -auto it- interaction
                     time.sleep(2)
                     if not load_files:
-                        raise Exception("Problem to send file attach. Aborting message.")
+                        raise Exception("[x] Problem to send file attach. Aborting message.")
 
                     # if after of 5 attempts is even loading, cancel send.
                     for attempt in range(0, 5):
                         if not ChatBoxHandle.is_loading_mode():
                             break
-                        print("[-]Trying to send...", attempt)
+                        print("[-] Trying to send...", attempt)
                         time.sleep(5)
                         if attempt  == 5:
                             raise Exception('[x] Error to attach file.')
@@ -242,11 +267,11 @@ def send_to(contact, message, attachment):
             time.sleep(1)
             ChatBoxHandle.confirm_send() # provisory send for files that not support comments. (videos)
         else:
-            raise Exception("Problem to send message after load files")
+            raise Exception("[x] Problem to send message after load files")
         print("[+]Message sent!")
         return True
     except:
-        print("[x]Error to send message:")
+        print("[Aborted!] Error to send message:")
         print(os.sys.exc_info())
         return False
 
@@ -259,24 +284,24 @@ def send_to_all(message, attachment=None):
     print("[+] Send_to_all finished...")
 
 def load_contacts(filename):
-    # Load contacts from .csv
+    # Load contacts from .csv and apply normalization
     print("[+]Loading contacts...")
     with open(filename, "r") as file:
         file = file.read().split('\n')
         delimiter = ','
         file_content = list(map(lambda c: c.split(delimiter), file))
-        header = file_content[0]
+        columns = file_content[0]
         for idx, row in enumerate(file_content[1:]):
-            if row and len(row) == len(header):
+            if row and len(row) == len(columns):
                 contacts[idx] = {}
-                contacts[idx].update({header:info.strip() for header, info in zip(header, row)})
+                contacts[idx].update({columns:info.strip() for columns, info in zip(columns, row)})
 
     print(f"[+]{len(contacts)} contacts loaded.")
-
+    return True
 
 if __name__ == '__main__':
     load_contacts("contacts.csv")
-    whatsapp_login(headless=False)
+    #whatsapp_login(headless=False)
     while True:
         try:
             eval(input("Debug console:"))
